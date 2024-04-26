da82ecbfdcb1e0fc5ac1ffb9c739b8654d83c418
@hexlet/cli <support@hexlet.io>
2024-04-26 23:58:02 +0300

    submit java-spring-ru/rest-api

---
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -0,0 +1,39 @@
+package exercise.controller.users;
+
+import exercise.Data;
+import exercise.model.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// BEGIN
+@RestController
+@RequestMapping("/api/users/{id}/posts")
+public class PostsController {
+    List<Post> posts = Data.getPosts();
+
+    @GetMapping
+    public List<Post> index(@PathVariable Integer id) {
+        return posts.stream().filter(post -> post.getUserId() == id).toList();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@PathVariable Integer id, @RequestBody Post data) {
+        var post = new Post();
+        post.setTitle(data.getTitle());
+        post.setBody(data.getBody());
+        post.setSlug(data.getSlug());
+        post.setUserId(id);
+        posts.add(post);
+        return post;
+    }
+}
+// END
