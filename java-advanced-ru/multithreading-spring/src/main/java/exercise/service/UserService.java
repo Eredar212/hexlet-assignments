db8a1371858c6576a6331da81b20e4695a71b7b2
@hexlet/cli <support@hexlet.io>
2024-05-27 22:36:56 +0300

    submit java-advanced-ru/multithreading-spring

---
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/service/UserService.java
@@ -0,0 +1,42 @@
+package exercise.service;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Flux<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    // BEGIN
+    public Mono<User> save(User user) {
+        return userRepository.save(user);
+    }
+    public Mono<User> update(User user, Long id) {
+        var userFromDb = userRepository.findById(id);
+        return userFromDb.flatMap(u -> {
+            u.setEmail(user.getEmail());
+            u.setFirstName(user.getFirstName());
+            u.setLastName(user.getLastName());
+            return userRepository.save(u);
+        });
+    }
+    public Mono<Void> delete(Long id) {
+        return userRepository.deleteById(id);
+    }
+    public Mono<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+    // END
+}
